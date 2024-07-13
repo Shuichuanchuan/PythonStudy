3573f80d9b3937e750a4e3da28587b717d20f086
shui <shuiwater@outlook.com>
2024-07-13 22:18:17 +0800

    input()，int使用

--- 7chapter用户输入和while循环/7.2/counting.py
+++ 7chapter用户输入和while循环/7.2/counting.py
@@ -1,1 +1,14 @@
 # 使⽤ while 循环
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+# 让⽤户选择何时退出
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
+# 使⽤标志
